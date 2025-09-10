16c77f4157b17f89822936b83b5233c08be9a519
Alex141994 <odnvst@yandex.ru>
2025-09-10 18:04:45 +0300

    An intermediate version, the attempt to organize a monotoring cycle.

--- inc/monitoring.h
+++ inc/monitoring.h
@@ -5,6 +5,39 @@
 #include <QTimer>
 #include <QSerialPort>
 
+enum pcs_consts{
+    START_POLL,
+    EQUIP_START_RESP,
+    FIRST_SNSR_Q,
+    FIRST_SNSR_RESP,
+    SECOND_SNSR_Q,
+    SECOND_SNSR_RESP,
+    THIRD_SNSR_Q,
+    THIRD_SNSR_RESP,
+    FIRST_VLV_CMD,
+    FIRST_VLV_RESP,
+    SECOND_VLV_CMD,
+    SECOND_VLV_RESP,
+    THIRD_VLV_CMD,
+    THIRD_VLV_RESP,
+    FOURTH_VLV_CMD,
+    FOURTH_VLV_RESP,
+    FIRST_SNSR_POLL_Q,
+    FIRST_SNSR_POLL_RESP,
+    SECOND_SNSR_POLL_Q,
+    SECOND_SNSR_POLL_RESP,
+    THIRD_SNSR_POLL_Q,
+    THIRD_SNSR_POLL_RESP,
+    FIRST_VLV_POLL_CMD,
+    FIRST_VLV_POLL_RESP,
+    SECOND_VLV_POLL_CMD,
+    SECOND_VLV_POLL_RESP,
+    THIRD_VLV_POLL_CMD,
+    THIRD_VLV_POLL_RESP,
+    FOURTH_VLV_POLL_CMD,
+    FOURTH_VLV_POLL_RESP
+};
+
 class monitoring: public QObject
 {
     Q_OBJECT
@@ -13,7 +46,13 @@ public:
 private:
     QTimer *mtr;
     int pollNSend();
-    QSerialPort sprt;
+    QSerialPort *sprt;
+    void poll2(QSerialPort::DataBits db);
+    int pollCycle();
+    int procError();
+    int pollCycleJumps(pcs_consts pcsc);
+    enum pcs_consts pcsc;
+    int snsMeanArray[3];
 };
 
 #endif // MONITORING_H
--- src/monitoring.cpp
+++ src/monitoring.cpp
@@ -8,9 +8,44 @@ monitoring::monitoring(QObject *parent):
     connect(mtr, &QTimer::timeout, this, &monitoring::pollNSend);
     mtr->setInterval(1000);
     mtr->start();
+    sprt->setBaudRate(115200);
 }
 
 int monitoring::pollNSend() {
+//    connect(test_tmr, &QTimer::timeout, this, &master::sendPbData);
+    pcsc = FIRST_SNSR_POLL_RESP;
+    connect(sprt, &QSerialPort::errorOccurred, this, &monitoring::procError);
+    connect(sprt, &QSerialPort::readyRead, this, &monitoring::pollCycle);
+//    QByteArray wBuff = {0x2, 0x5, 0x7};
+//    sprt->write(wBuff);
+    return 0;
+}
 
+int monitoring::pollCycle(){
+    QByteArray data = sprt->readAll();
+    switch(pcsc){
+        case FIRST_SNSR_POLL_RESP:
+            snsMeanArray[1] = data[5]*256 +data[6];
+            pcsc = SECOND_SNSR_POLL_RESP;
+//            QByteArray wBuff = {0x2, 0x5, 0x7};
+//            sprt->write(wBuff);
+        break;
+        case SECOND_SNSR_POLL_RESP:
+            snsMeanArray[1] = data[5]*256 +data[6];
+            pcsc = SECOND_SNSR_POLL_RESP;
+//            QByteArray wBuff = {0x2, 0x5, 0x7};
+//            sprt->write(wBuff);
+        break;
+    default: break;
+    }
+
+    return 0;
+}
+
+void monitoring::poll2(QSerialPort::DataBits db){
+
+}
+
+int monitoring::procError(){
     return 0;
 }
